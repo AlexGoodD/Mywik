92019120e5364555b3283645a1a3c5f845e671e8
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-30 20:48:28 -0600

    Vistas básicas de inicio, autenticación y dashboard

---
+++ src/views/authView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="auth-container">
+    <component
+      :is="isLogin ? LoginPage : RegisterPage"
+      @switchToRegister="toggleView"
+      @switchToLogin="toggleView"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import RegisterPage from "../components/authPage/registerPage.vue";
+import LoginPage from "../components/authPage/loginPage.vue";
+
+const isLogin = ref(true);
+
+const toggleView = () => {
+  isLogin.value = !isLogin.value;
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/dashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Bienvenido, {{ userName }}</h1>
+    <Calendar />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUser } from "../composables/useUser";
+import Calendar from "../components/Schedule/tableCalendar.vue";
+
+const { userName } = useUser();
+</script>
