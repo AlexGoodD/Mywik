6f7f2f976c55209dab42d97e4af883ac22308388
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-30 23:33:37 -0600

    Navbar implementado en dashboard

--- src/views/dashboardView.vue
+++ src/views/dashboardView.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
+    <Navbar class="navbar-left" />
     <h1>Bienvenido, {{ userName }}</h1>
-    <Calendar />
+    <gridSchedule />
+    <ButtonCreate @click="showForm = true" />
+    <FormSchedule v-if="showForm" @close="showForm = false" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { useUser } from "../composables/useUser";
-import Calendar from "../components/Schedule/tableCalendar.vue";
+import Navbar from "../components/appNavbar.vue";
+import gridSchedule from "../components/Schedule/gridSchedule.vue";
+import ButtonCreate from "../components/items/createButton.vue";
+import FormSchedule from "../components/Schedule/formSchedule.vue";
 
 const { userName } = useUser();
+const showForm = ref(false);
 </script>
+
+<style scoped>
+.navbar-left {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
