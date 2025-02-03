09bae407ed2987492d97e5fb1495a1bec8b36fbf
AlexGoodD <alejandrochavarb@gmail.com>
2025-02-03 01:08:31 -0600

    Componentes de botones outlined & fill

---
+++ src/components/items/fillButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="create-button" @click="handleClick">
+    <i :class="iconClass" id="icon-create_button"></i> {{ props.buttonText }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ buttonText: string; iconClass: string }>();
+const emits = defineEmits(["click"]);
+
+function handleClick() {
+  emits("click");
+}
+</script>
+
+<style scoped>
+.create-button {
+  width: auto;
+  border-radius: 0.5rem;
+  background-color: #000000;
+  color: white;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-size: 0.7rem;
+  padding: 0.5rem 1rem;
+}
+
+.create-button:hover {
+  background-color: #4f4f4f;
+}
+
+#icon-create_button {
+  margin-right: 0.5rem;
+}
+</style>
---
+++ src/components/items/outlinedButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="create-button" @click="handleClick">
+    <i :class="iconClass" id="icon-create_button"></i> {{ props.buttonText }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ buttonText: string; iconClass: string }>();
+const emits = defineEmits(["click"]);
+
+function handleClick() {
+  emits("click");
+}
+</script>
+
+<style scoped>
+.create-button {
+  width: auto;
+  border-radius: 0.5rem;
+  border: 1px solid #d9d9d9;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: white;
+  font-size: 0.7rem;
+  padding: 0.5rem 1rem;
+}
+
+/*.create-button:hover {
+}*/
+
+#icon-create_button {
+  margin-right: 0.5rem;
+}
+</style>
