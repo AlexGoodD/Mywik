0dd2b95629382906228c112ae2c4b9fc1996f157
AlexGoodD <alejandrochavarb@gmail.com>
2025-02-03 01:10:45 -0600

    Pagina 404 agregada

--- src/components/utils/notFound.vue
+++ src/components/utils/notFound.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <h1>404 - Página no encontrada</h1>
     <p>Lo sentimos, la página que estás buscando no existe.</p>
-    <router-link to="/">Volver al inicio</router-link>
+    <router-link to="/dashboard">Volver al inicio</router-link>
   </div>
 </template>
 
