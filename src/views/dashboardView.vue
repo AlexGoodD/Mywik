1af4d2be2fb4e3a1d7b520fe688c0c18e4dd2dd5
AlexGoodD <alejandrochavarb@gmail.com>
2025-02-03 01:13:19 -0600

    Import de dashboardPage

--- src/views/dashboardView.vue
+++ src/views/dashboardView.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <h1>Bienvenido, {{ userName }}</h1>
-    <gridSchedule />
-    <ButtonCreate @click="showForm = true" />
-    <FormSchedule v-if="showForm" @close="showForm = false" />
+    <dashboardPage class="dashboard-container" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { useUser } from "../composables/useUser";
-import gridSchedule from "../components/Schedule/gridSchedule.vue";
-import ButtonCreate from "../components/items/createButton.vue";
-import FormSchedule from "../components/Schedule/formSchedule.vue";
-
-const { userName } = useUser();
-const showForm = ref(false);
+import dashboardPage from "../components/dashboard/dashboardPage.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.dashboard-container {
+}
+</style>
